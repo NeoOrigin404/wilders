fea33eff402243059f9f773631741409c323d44b
Julien PICART <julienpicart01@gmail.com>
2024-12-09 10:40:06 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+import chalk from "chalk";
+
+const lisa = "Lisa";
+const brayan = "Brayan";
+const theophile = "Théophile";
+const julian = "Julian";
+
+console.log(
+  chalk.magenta(lisa),
+  chalk.blue(brayan),
+  chalk.green(theophile),
+  chalk.red(julian)
+);
